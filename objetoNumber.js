f5056baa4b4fb75887fe494737868affd78aa0ec
kevQL <unajmakev@gmail.com>
2018-12-26 14:33:12 -0500

    Objto Number js

---
+++ objetoNumber.js
@@ -0,0 +1,14 @@
+
+var a = 100.123141;
+var b = new Number(20);
+
+console.log(a);
+console.log(b);
+
+console.log( a.toString() );
+
+console.log( a.toFixed(2) )
+
+console.log( a.toPrecision(2) )
+
+console.log( b.valueOf() );
