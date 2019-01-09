f4f1ae04877d9178cb83a9240d63a4819806ea04
kevQL <unajmakev@gmail.com>
2019-01-09 10:30:45 -0500

    Bloqueando click and capturtando el texto seleccionado en js

---
+++ bloqueClick.js
@@ -0,0 +1,21 @@
+// enlaza con index4.html
+
+
+//cuando le den click
+document.onmousedown = function( arg ){
+    
+    if( arg.button === 2 ){
+        alert("Teclado bloquedo");
+        return;
+    }
+    console.log( "No hay problema!!" )
+    console.log( arg )
+
+}
+
+//cuando suelten el click
+document.onmouseup = function( arg ){
+    var texto = window.getSelection().toString(); // Captura la selección de texto
+    console.log( texto );
+    console.log( arg );
+}
